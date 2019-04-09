7eb1785141d73c18dd134af2e2389a97659889fe
Jaskiran Kaur <jkaur372@my.centennialcollege.ca>
2019-04-09 03:05:13 -0400

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,5 @@
 module.exports = {
 //"URI" : "mongodb://localhost/sidharth",
-//"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
-"URI":"mongodb://sidnew:sid1234@ds058739.mlab.com:58739/sidharth",
+"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
 "secret": "SomeSecret"
 }
